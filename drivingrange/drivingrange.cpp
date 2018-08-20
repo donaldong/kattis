a26efa64af7c82fde30caf982153ab7b9f2806a3
Donald Dong <xdong@csumb.edu>
2018-08-20 18:53:47 +0000

    Driving Range [2]
    Change algo to Union-Find
    Ref
    https://github.com/mpfeifer1/Kattis/blob/master/drivingrange.cpp

--- drivingrange/drivingrange.cpp
+++ drivingrange/drivingrange.cpp
@@ -1,52 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using uint = unsigned int;
-
-uint INF = -1;
 int N, M;
-// Grid
-vector<vector<uint>> G;
+// Parents
+vector<int> P;
 
-void debug() {
-  cout << endl;
-  for (auto &row : G) {
-    for (auto r : row) {
-      cout << int(r) << " ";
-    }
-    cout << endl;
-  }
-}
-
-void floyd_warshall() {
-  for (int k = 0; k < N; ++k) {
-    for (int i = 0; i < N; ++i) {
-      if (k >= i) continue;
-      for (int j = 0; j < N; ++j) {
-        if (j >= k) break;
-        if (G[i][k] != INF && G[k][j] != INF) {
-          G[i][j] = min(G[i][j], max(G[i][k], G[k][j]));
-        }
-      }
-    }
-  }
+int find(int n) {
+  if (P[n] == -1) return n;
+  P[n] = find(P[n]);
+  return P[n];
 }
 
 int main() {
   cin >> N >> M;
-  G = vector<vector<uint>>(N);
-  for (int i = 1; i < N; ++i) G[i] = vector<uint>(i, -1);
+  P = vector<int>(N, -1);
+  vector<tuple<int, int, int>> L(M);
   for (int i = 0; i < M; ++i) {
-    uint a, b, c;
+    int a, b, c;
     cin >> a >> b >> c;
-    if (a == b) continue;
-    if (b > a) swap(a, b);
-    G[a][b] = min(G[a][b], c);
+    L[i] = make_tuple(c, a, b);
   }
-  floyd_warshall();
-  uint res = 0;
-  for (auto &row : G) for (auto &d : row) res = max(res, d);
-  if (res == INF) cout << "IMPOSSIBLE" << endl;
-  else cout << res << endl;
+  sort(L.begin(), L.end());
+  int res = 0;
+  for (auto &l : L) {
+    int a, b, c;
+    tie(c, a, b) = l;
+    int pa = find(a), pb = find(b);
+    if (pa != pb) {
+      P[pb] = pa;
+      res = max(res, c);
+    }
+  }
+  for (int i = 1; i < N; ++i) {
+    if (find(i) != find(i - 1)) {
+      cout << "IMPOSSIBLE" << endl;
+      return 0;
+    }
+  }
+  cout << res << endl;
   return 0;
 }
