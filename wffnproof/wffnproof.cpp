b65e3cadfeb1bc8f8b7c4ca2793e9c8c59d2d9eb
Donald Dong <xdong@csumb.edu>
2018-08-24 01:01:51 +0000

    WFF 'N PROOF [2]
    Fixing WA
    handle invalid var names

--- wffnproof/wffnproof.cpp
+++ wffnproof/wffnproof.cpp
@@ -9,47 +9,34 @@ bool is_opt(char c) {
     c == 'E';
 }
 
-string to_postfix(string &str) {
+bool is_var(char c) {
+  return c == 'p' ||
+    c == 'q' ||
+    c == 'r' ||
+    c == 's' ||
+    c == 't';
+}
+
+string solve(string &str) {
   size_t N = 0;
   string vars, opts;
   for (char c : str) {
     if (c == 'N') ++N;
     else if (is_opt(c)) opts += c; 
-    else vars += c;
+    else if (is_var(c)) vars += c;
   }
-  if (vars.empty()) return vars;
+  if (vars.empty()) return "no WFF possible";
   if (vars.size() > opts.size() + 1) vars = vars.substr(0, opts.size() + 1);
-  if (vars.size() <= opts.size()) opts = opts.substr(0, vars.size() - 1);
+  else if (vars.size() <= opts.size()) opts = opts.substr(0, vars.size() - 1);
   N = min(N, vars.size());
-  vars += string(N, 'N');
-  return vars + opts;
-}
-
-string to_prefix(string s) {
-  if (s.empty()) return "no WFF possible";
-  stack<string> S;
-  for (char c : s) {
-    if (is_opt(c)) {
-      if (c == 'N') {
-        S.top() = string(1, 'N') + S.top();
-      } else {
-        string res(1, c);
-        res += S.top(), S.pop();
-        res += S.top(), S.pop();
-        S.push(res);
-      }
-    } else {
-      S.push(string(1, c));
-    }
-  }
-  return S.top();
+  return opts + string(N, 'N') + vars;
 }
 
 int main() {
   string line;
   while (getline(cin, line)) {
     if (line == "0") break;
-    cout << to_prefix(to_postfix(line)) << endl;
+    cout << solve(line) << endl;
   }
   return 0;
 }
