4b2679d5942c35f1c061923de83bd1aadf898c58
Donald Dong <xdong@csumb.edu>
2018-08-26 05:05:18 +0000

    Setnja
    Fixing WA

--- setnja/setnja.py
+++ setnja/setnja.py
@@ -2,11 +2,14 @@ line = input()
 res = 1
 r = 1
 for c in line:
-    if c == 'L':
+    if c == 'P':
+        continue
+    elif c == 'L':
         res *= 2
     elif c == 'R':
         res = res * 2 + r
     else:
-        res =  2 * res * (1 + r)
+        t = 2 * res
+        res += 2 * t + r
         r *= 3
 print(res)
