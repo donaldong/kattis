20822be0f42ee672a5dfc7600930ae40ce3b0d64
Donald Dong <xdong@csumb.edu>
2018-08-28 16:28:26 +0000

    Room Painting [2]
    Fixing WA

--- roompainting/roompainting.cpp
+++ roompainting/roompainting.cpp
@@ -4,10 +4,13 @@ using namespace std;
 using vi = vector<int>;
 using ll = long long;
 
+int INF = 1e9;
+
 ll solve(vi &N, vi &M) {
   ll res = 0;
-  sort(N.begin(), N.end());
   sort(M.begin(), M.end());
+  for (auto &n : N) if (n < M[0]) n = INF;
+  sort(N.begin(), N.end());
   for (size_t i = M.size(); i-- > 0;) {
     int m = M[i];
     int cur = N.back();
