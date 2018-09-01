fedadcfe4c20b6da0e1f48b5f2c5f820a60f7b11
Donald Dong <xdong@csumb.edu>
2018-09-01 19:59:47 +0000

    Thesaurus
    https://open.kattis.com/problems/thesaurus

--- thesaurus/thesaurus.cpp
+++ thesaurus/thesaurus.cpp
@@ -12,16 +12,40 @@ hmap M;
 struct node {
   vn neigh;
   bool visited;
+  size_t length;
+  size_t min_length = -1;
+  node() {}
+  node(string &str) {
+    length = str.size();
+  }
 };
 
 node* get_node(string &s) {
   if (M.find(s) != M.end()) return M[s];
-  M[s] = new node();
+  M[s] = new node(s);
   return M[s];
 }
 
 void connect(node *a, node *b) {
-  a->
+  a->neigh.push_back(b);
+  b->neigh.push_back(a);
+}
+
+size_t dfs(node *n) {
+  if (n->min_length != -1) return n->min_length;
+  n->min_length = min(n->min_length, n->length);
+  for (auto c : n->neigh) {
+    n->min_length = min(n->min_length, dfs(c));
+  }
+  return n->min_length;
+}
+
+size_t solve(vs &N) {
+  size_t res = 0;
+  for (auto &word : N) {
+    res += dfs(get_node(word));
+  }
+  return res;
 }
 
 int main() {
