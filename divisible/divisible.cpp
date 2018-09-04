9c647ba0738b13bf17972f1e686d45965a5491c0
Donald Dong <xdong@csumb.edu>
2018-09-04 23:51:53 +0000

    Divisible Subsequences [5]
    Fixing TLE
    for each position, check how many new results can be added if it ends here

--- divisible/divisible.cpp
+++ divisible/divisible.cpp
@@ -3,33 +3,23 @@ using namespace std;
 
 using Num = int;
 using Nums = vector<Num>;
-using SumMap = unordered_map<Num, Num>;
-
-Num solve(Nums &N, int d) {
-  Num res = 0;
-  for (size_t i = 0; i < N.size(); ++i) {
-    Num sum = N[i];
-    if (!N[i]) ++res;
-    for (size_t j = i + 1; j < N.size(); ++j) {
-      sum += i;
-      sum %= d;
-      if (!sum) ++res;
-    }
-  }
-  return res;
-}
 
 int main() {
   int c, d, n;
   cin >> c;
   while (c--) {
     cin >> d >> n;
-    Nums N(n);
-    for (auto &num : N) {
-      cin >> num;
-      num %= d;
+    Nums S(d, 0);
+    int res = 0;
+    int sum = 0;
+    while (n--) {
+      int x;
+      cin >> x;
+      ++S[sum];
+      sum = (sum + x) % d;
+      res += S[sum];
     }
-    cout << solve(N, d) << endl;
+    cout << res << endl;
   }
   return 0;
 }
