d25cc3d0f6a8dc88274025dfdfeaf4c806a05b57
Donald Dong <xdong@csumb.edu>
2018-09-04 15:55:41 +0000

    Batmanacci [2]
    Fixing WA
    wrong index

--- batmanacci/batmanacci.cpp
+++ batmanacci/batmanacci.cpp
@@ -8,8 +8,8 @@ ull MAX_F = 1e18;
 vull F;
 
 char solve(ull n, ull k) {
-  if (n == 0) return 'N';
-  if (n == 1) return 'A';
+  if (n == 1) return 'N';
+  if (n == 2) return 'A';
   if (k <= F[n - 2]) return solve(n - 2, k);
   return solve(n - 1, k - F[n - 2]);
 }
@@ -17,7 +17,7 @@ char solve(ull n, ull k) {
 int main() {
   ull n, k;
   cin >> n >> k;
-  F = vull(2, 1);
+  F = vull(3, 1);
   for (ull i = 0; i < n; ++i) {
     size_t k = F.size() - 1;
     if (F[k - 1] > MAX_F) break;
