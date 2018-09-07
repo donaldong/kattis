329950678beeec35d42ef0052feb336804039517
Donald Dong <xdong@csumb.edu>
2018-09-07 19:45:18 +0000

    Good Coalition
    https://open.kattis.com/problems/goodcoalition

--- goodcoalition/goodcoalition.cpp
+++ goodcoalition/goodcoalition.cpp
@@ -3,34 +3,63 @@ using namespace std;
 
 using ld = long double;
 using vld = vector<ld>;
+using vi = vector<int>;
+using v2i = vector<vi>;
 
-int MAX_SEATS = 151;
+struct comb {
+  vi list;
+  int sum = 0;
 
-ld solve(vld &P, int s) {
-  if (P[s] > -0.5) return P[s];
-  ld res = 0;
-  for (int i = 1; 2 * i < s; ++i) {
-    res = max(res, solve(P, i) * solve(P, s - i));
+  comb() {}
+  comb(const comb &c) {
+    list = c.list;
+    sum = c.sum;
   }
-  cout << s << " " << res << endl;
-  P[s] = res;
+  void add(int i, int n) {
+    list.push_back(i);
+    sum += n;
+  }
+};
+
+ld calc(vld &P, comb &c) {
+  ld res = 1.0;
+  for (auto i : c.list) res *= P[i];
+  return res;
+}
+
+ld solve(vi &S, vld &P) {
+  vector<comb> C;
+  ld res = 0;
+  for (size_t i = 0; i < S.size(); ++i) {
+    size_t j = C.size();
+    for (size_t k = 0; k < j; ++k) {
+      comb new_comb(C[k]);
+      new_comb.add(i, S[i]);
+      if (new_comb.sum < 76) {
+        C.push_back(new_comb);
+      } else {
+        res = max(res, calc(P, new_comb));
+      }
+    }
+    comb c;
+    c.add(i, S[i]);
+    C.push_back(c);
+  }
   return res;
 }
 
 int main() {
-  int t, n, s;
-  ld p;
+  int t, n;
   cin >> t;
   while (t--) {
     cin >> n;
-    vld P(MAX_SEATS, -1);
-    while (n--) {
-      cin >> s >> p;
-      P[s] = max(P[s], p / 100.0);
+    vi S(n);
+    vld P(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> S[i] >> P[i];
+      P[i] /= 100.0;
     }
-    p = 0;
-    for (int i = 76; i <= 150; ++i) p = max(p, solve(P, i));
-    cout << p * 100.0 << endl;
+    printf("%.1Lf\n", solve(S, P) * 100.0);
   }
   return 0;
 }
