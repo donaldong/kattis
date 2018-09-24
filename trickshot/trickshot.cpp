8267efe6ecd23421bb8e0c703d0446bd773742dc
Donald Dong <xdong@csumb.edu>
2018-09-23 19:59:17 -0700

    Trick Shot
    https://open.kattis.com/problems/trickshot
    Tags
    - Geometry

--- trickshot/trickshot.cpp
+++ trickshot/trickshot.cpp
@@ -2,68 +2,72 @@
 using namespace std;
 
 using ld = long double;
+ld EPS = 1e-8;
 
 struct pt {
   ld x, y;
   pt() {}
   pt(ld x, ld y) : x(x), y(y) {}
-  pt operator-(pt &p) {
-    return pt(x - p.x, y - p.y);
-  }
+  pt operator-(pt &p) { return pt(x - p.x, y - p.y); }
+  ld arg() { return atan2(y, x); }
 };
 
-pt move_back(pt &a, pt &dir, ld r) {
-  ld theta = atan2(dir.y, dir.x);
-  return pt(a.x - r * cos(theta), a.y - r * sin(theta));
+pt forward(pt &center, pt &dir, ld dis) {
+  ld a = dir.arg();
+  return pt(center.x + dis * cos(a), center.y + dis * sin(a));
 }
 
-void debug(pt &p, string s="") {
-  cout << s << endl;
-  printf(" [%.2Lf, %.2Lf] ", p.x, p.y);
-  cout << endl;
+ld fix(ld a) {
+  ld two_pi = 2.0 * M_PI;
+  while (a > two_pi) a -= two_pi;
+  while (a < 0) a += two_pi;
+  return a;
 }
 
-ld to_deg(ld n) {
-  ld res = n / M_PI * 180.0;
-  if (res < 0) res += 360;
-  return res;
-}
+ld dot(pt &a, pt &b) { return a.x * b.x + a.y * b.y; }
 
-bool possible(pt &A, pt &B, pt &C) {
-  ld c = atan2(C.y, C.x);
-  ld b = atan2(-B.y, -B.x);
-  if (b > 0) return false;
-  ld a = atan2(-A.y, -A.x);
-  ld ba = a - b;
-  ld cb = b - c;
-  ba = to_deg(ba);
-  cb = to_deg(cb);
-  if (ba > 90 || cb > 90) return false;
-  return abs(ba - cb) < 0.005;
+ld dist(pt &a, pt &b) { return hypot(a.x - b.x, a.y - b.y); }
+
+bool collide(pt &A, pt &B, ld r, pt &C) {
+  pt AC = C - A, AB = B - A, P;
+  ld dx = B.x - A.x, dy = B.y - A.y;
+  ld proj = dot(AC, AB) / dot(AB, AB); 
+  if (proj < 0) P = A;
+  else if (proj > 1) P = B;
+  else {
+    P.x = A.x + proj * dx;
+    P.y = A.y + proj * dy;
+  } 
+  return dist(C, P) < 2 * r - EPS;
 }
 
 int main() {
   pt A, B, C;
-  ld w, l, h, r, a, d;
+  ld w, l, h, r, a, d, theta, x;
   cin >> w >> l >> r >> A.x >> A.y >> B.x >> B.y >> C.x >> C.y >> h;
-  pt L(0, l), R(w, l);
-  pt CR = R - C, BL = L - B;
-  pt contactCR = move_back(C, CR, 2 * r);
-  pt dirA = contactCR - A;
-  pt contactAQ = move_back(A, dirA, 2 * r);
-  pt contactBL = move_back(B, BL, 2 * r);
-  pt dirQ = contactBL - contactAQ;
-  bool f = false;
-  for (ld x = r; x < w - r; x += 0.0005) {
-    pt F(x, h);
-    F = contactAQ - F;
-    if (possible(F, dirA, dirQ)) {
-      a = atan2(F.y, F.x);
-      printf("%.2Lf %.2Lf\n", x, to_deg(a));
-      f = true;
-      break;
-    }
+  pt Bp(0, l), Cp(w, l);
+  pt BpB = B - Bp, CpC = C - Cp;
+  pt Qpp = forward(B, BpB, 2 * r), Ap = forward(C, CpC, 2 * r);
+  pt ApA = A - Ap;
+  pt Qp = forward(A, ApA, 2 * r);
+  pt QpQpp = Qpp - Qp;
+  x = ApA.arg(), a = QpQpp.arg();
+  theta = x + x - a;
+  pt Q((h - Qp.y) / tan(theta) + Qp.x, h);
+  if (
+    // Incident angle > 90 deg
+    dot(ApA, CpC) < 0 ||
+    dot(ApA, QpQpp) < 0 ||
+    dot(BpB, QpQpp) > 0 ||
+    // Out of bound
+    Q.x < r || Q.x > w - r ||
+    // Collide on path Q -> Qp
+    collide(Q, Qp, r, B) || collide(Q, Qp, r, C)
+  ) {
+    printf("impossible\n");
+  } else {
+    theta = fix(theta) - M_PI;
+    printf("%.2Lf %.2Lf\n", Q.x, theta / M_PI * 180.0);
   }
-  if (!f) cout << "impossible" << endl;
   return 0;
 }
