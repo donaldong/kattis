11d89d940c11b24a9592269a7c49850ecaa730d6
Donald Dong <xdong@csumb.edu>
2018-09-29 00:37:27 -0700

    Avoiding the Apocalypse
    https://open.kattis.com/problems/avoidingtheapocalypse
    Tags
    - Flow

--- avoidingtheapocalypse/avoidingtheapocalypse.cpp
+++ avoidingtheapocalypse/avoidingtheapocalypse.cpp
@@ -2,90 +2,65 @@
 using namespace std;
 
 struct edge {
-  int n, p, t;
+  int n, from, a, b, flow;
   edge() {}
-  edge(int n, int p, int t) : n(n), p(p), t(t) {}
+  edge(int n, int flow, int from, int a, int b) : 
+    n(n), flow(flow), from(from), a(a), b(b) {}
 };
 
+using vb = vector<bool>;
 using vi = vector<int>;
 using v2i = vector<vi>;
 using ve = vector<edge>;
 using v2e = vector<ve>;
-using vb = vector<bool>;
 using ti2 = tuple<int, int>;
-int MAX_T = 101;
+int INF = 1e9;
+int MAX_T;
 
-void debug(v2i &G) {
-  cout << endl;
-  for (auto &row : G) {
-    for (auto &cell : row) {
-      if (cell == INF) cout << "I ";
-      else cout << cell << " "; 
-    }
-    cout << endl;
-  }
-  cout << endl;
+int index(int n, int t) {
+  return n * MAX_T + t;
 }
 
-v2i build(v2e &G, vb &F, int cur, int g, int s) {
-  // sink, target, remaining time
-  size_t size = n * MAX_T + 2; 
-  int sink = size - 2, target = size - 1, rt;
-  v2i RG(size, vi(size, 0));
-  RG[sink][cur] = g;
-  for (size_t i = 0; i < G.size(); ++i) {
-    if (!F[i]) continue;
-    for (size_t t = 0; t < MAX_T; ++t) {
-      RG[index(i, t)][target] = INF;
-    }
-  }
-  queue<tuple<int, int>> Q;
-  RG[sink][index(cur, 0)] = g;
-  Q.emplace(s, cur);
-  while (!Q.empty()) {
-    tie(rt, cur) = Q.front(); Q.pop(); 
-  }
-  for (size_t i = 0; i < G.size(); ++i) {
-    for (auto &e : G[i]) {
-      rt = s - D[i];
-      printf("%d %d %d\n", i, e.n, rt);
-      RG[i][e.n] = max((rt - e.t + 1) * e.p, 0);
-    }
-  }
-  return RG;
+void connect(v2e &G, int a, int b, int flow) {
+  int i = G[a].size(), j = G[b].size();
+  G[a].emplace_back(b, flow, a, i, j);
+  G[b].emplace_back(a, 0, b, j, i);
 }
 
-bool bfs(v2i &G, int cur, int t, vi &P) {
+bool bfs(v2e &G, int cur, int t, ve &P) {
   vb V(G.size(), false);
   queue<int> Q;
   Q.push(cur);
   V[cur] = true;
   while (!Q.empty()) {
     cur = Q.front(); Q.pop();
-    for (size_t i = 0; i < G.size(); ++i) {
-      if (G[cur][i] <= 0 || V[i]) continue;
-      Q.push(i);
-      P[i] = cur;
-      V[i] = true;
+    for (auto &e : G[cur]) {
+      if (e.flow <= 0 || V[e.n]) continue;
+      Q.push(e.n);
+      P[e.n] = e;
+      V[e.n] = true;
     }
   }
   return V[t];
 }
 
-int max_flow(v2i &G, int s, int t) {
-  vi P(G.size());
+int max_flow(v2e &G, int s, int t) {
+  ve P(G.size());
   int res = 0, a, b;
+  edge e;
   while (bfs(G, s, t, P)) {
     int flow = INF;
     for (b = t; b != s;) {
-      a = P[b];
-      flow = min(flow, G[a][b]);
+      e = P[b];
+      a = e.from;
+      flow = min(flow, G[a][e.a].flow);
       b = a;
     }
     for (b = t; b != s;) {
-      a = P[b];
-      G[a][b] -= flow;
-      G[b][a] += flow;
+      e = P[b];
+      a = e.from;
+      G[a][e.a].flow -= flow;
+      G[b][e.b].flow += flow;
       b = a;
     }
     res += flow;
@@ -94,27 +69,33 @@ int max_flow(v2i &G, int s, int t) {
 }
 
 int main() {
-  int T, n, i, g, s, m, x, r, a, b, p, t;
+  int T, n, i, j, g, s, m, x, r, a, b, p, t;
   cin >> T;
   while (T--) {
     cin >> n >> i >> g >> s >> m; --i;
-    v2e G(n);
-    vb F(n, false);
+    MAX_T = s + 1;
+    v2e G(n * MAX_T + 2);
+    int sink = G.size() - 2, target = G.size() - 1;
+    connect(G, sink, index(i, 0), g);
     while (m--) {
-      cin >> a
+      cin >> a;
       --a;
-      F[a] = true;
+      for (i = 0; i < MAX_T; ++i) {
+        connect(G, index(a, i), target, INF);
+      }
     }
     cin >> r;
     while (r--) {
       cin >> a >> b >> p >> t;
       --a, --b;
-      G[a].emplace_back(b, p, t);
+      for (i = 0; (j = i + t) < MAX_T; ++i) {
+        connect(G, index(a, i), index(b, j), p);
+      }
     }
-    // Residual Graph 
-    v2i RG = build(G, F, i, g, s);
-    printf("%d\n", max_flow(RG, n * MAX_T, n * MAX_T + 1));
-    debug(RG);
+    for (i = 0; i < n; ++i) for (t = 0; t < s; ++t) {
+      connect(G, index(i, t), index(i, t + 1), INF);
+    }
+    printf("%d\n", max_flow(G, sink, target));
   }
   return 0;
 }
