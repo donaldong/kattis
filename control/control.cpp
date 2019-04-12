d189231e3b433f47b0b80ff7162a5d9aa19ec598
Donald Dong <xdong@csumb.edu>
2019-04-11 22:58:40 -0700

    Association for Control Over Minds
    - Medium
    - Union Find

--- control/control.cpp
+++ control/control.cpp
@@ -1,47 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define hmap unordered_map
-#define hset unordered_set
+typedef vector<int> vi;
+int MAX_M = 5e5;
+vi RANK(MAX_M + 1, 1), P(MAX_M + 1, -1);
 
-// max number of ingredients
-int MAX_M = 500001;
-vector<int> M(MAX_M, -1);
+int find(int n) {
+  if (P[n] == n || P[n] == -1) return n;
+  return P[n] = find(P[n]);
+}
 
-bool possible(vector<int> &I, int r) {
-  sort(I.begin(), I.end());
-  int k = 0;
-  hset<int> A;
-  for (int i : I) {
-    if (M[i] == -1) continue;
-    A.insert(M[i]);
+void join(int a, int b) {
+  if (RANK[a] >= RANK[b]) {
+    RANK[a] += RANK[b], RANK[b] = 0;
+    P[b] = a;
+  } else {
+    RANK[b] += RANK[a], RANK[a] = 0;
+    P[a] = b;
   }
-  for (int i = 0; i < MAX_M; ++i) {
-    if (M[i] == -1) continue;
-    if (k < I.size() && i == I[k]) {
-      ++k;
-      continue;
-    }
-    if (A.find(M[i]) != A.end()) return false;
+#ifdef DEBUG
+  printf("rank[%d] = %d, rank[%d] = %d\n",
+    a, RANK[a], b, RANK[b]);
+#endif
+}
+
+bool solve(vi &M) {
+  unordered_map<int, int> R;
+  for (int m : M) {
+    int p = find(m);
+    R[p] = RANK[p];
   }
-  for (int i : I) {
-    M[i] = r;
+  for (int m : M) --R[find(m)];
+  for (auto &e : R) if (e.second) return false;
+  for (int i = 1; i < M.size(); ++i) {
+    int a = find(M[0]), b = find(M[i]);
+    if (a != b) join(a, b);
   }
   return true;
 }
 
 int main() {
-  int n;
+  ios::sync_with_stdio(0), cin.tie(0);
+  int n, m, k, res = 0;
   cin >> n;
-  int res = 0;
   for (int i = 0; i < n; ++i) {
-    int k;
-    cin >> k;
-    // ingredients
-    vector<int> I(k);
-    for (auto &e : I) cin >> e;
-    if (possible(I, i)) ++res;
+    cin >> m;
+    vi M(m);
+    for (auto &e : M) cin >> e;
+    if (solve(M)) ++res;
   }
-  cout << res << endl;
+  printf("%d\n", res);
   return 0;
 }
