4b7f11f524d9b9e9b1822e1eeb3096dcac407305
Donald Dong <ddong@alumni.csumb.edu>
2022-03-21 02:42:59 -0700

    Add more solutions

---
+++ solutions/election2/a.cpp
@@ -0,0 +1,37 @@
+/**
+ * @author      Donald Dong
+ * @date        2022-03-21
+ * @tag         hash map
+ * @difficulty  1300
+ */
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(0); // Fast IO
+  int n, m;
+  cin >> n;
+  string s1, s2;
+  getline(cin, s1);
+  unordered_map<string, string> P;
+  while (n--) {
+    getline(cin, s1);
+    getline(cin, s2);
+    P[s1] = move(s2);
+  }
+  cin >> m;
+  getline(cin, s1);
+  unordered_map<string, int> C;
+  int maxCnt = 0, freq = 0;
+  string winner;
+  while (m--) {
+    getline(cin, s1);
+    auto& c = C[s1];
+    maxCnt = max(maxCnt, ++c);
+  }
+
+  for (const auto& [name, k] : C) {
+    if (k == maxCnt) ++freq, winner = name;
+  }
+  if (freq != 1) cout << "tie" << endl;
+  else cout << P[winner] << endl;
+}
---
+++ solutions/intervalscheduling/a.cpp
@@ -0,0 +1,27 @@
+/**
+ * @author      Donald Dong
+ * @date        2022-03-21
+ * @tag         greedy, sorting
+ * @difficulty  1300
+ */
+#include <bits/stdc++.h>
+using namespace std;
+using Pair = pair<int,int>;
+int main() {
+  ios_base::sync_with_stdio(false), cin.tie(0); // Fast IO
+  int N;
+  cin >> N;
+  vector<Pair> P(N);
+  for (auto& p : P) cin >> p.first >> p.second;
+  sort(P.begin(), P.end(), [](const Pair& a, const Pair& b) {
+    return a.second<b.second;
+  });
+  int t = 0, res = 0;
+  for (auto& p : P) {
+    if (p.first >= t) {
+      ++res;
+      t = p.second;
+    }
+  }
+  cout << res << endl;
+}
